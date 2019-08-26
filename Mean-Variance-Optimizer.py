8d76664c4be43a34144588756dcb2c193fb57f79
Jonathan Liesch <jonathanliesch@g.ucla.edu>
2019-08-25 21:05:24 -0700

    Added sharpe heatmap and CAL

--- Mean-Variance-Optimizer.py
+++ Mean-Variance-Optimizer.py
@@ -3,6 +3,8 @@ import pandas as pd
 import math
 import random
 from matplotlib import pyplot as plt
+import matplotlib.colors
+import matplotlib.cm
 
 asset_counter = 0
 class asset:
@@ -21,15 +23,14 @@ class asset:
             % {'id': self.id, 'name': str(self.name), 'mean': self.mean, 'std': self.std} \
                 + "#####################\n"
 
-
 class universe:
-    def __init__(self, names, means, std, cov):
+    def __init__(self, names, means, std, cov, rf):
         self.assets = []
         for i in range(len(names)):
             self.assets.append(asset(names[i], means[i], std[i]))
         self.mean_vec = means
         self.sigma = cov
-    
+        self.rf = rf
     
 portfolio_count = 0
 class portfolio:
@@ -42,6 +43,7 @@ class portfolio:
         self.mean = np.matmul(universe.mean_vec, weights)
         self.var = np.matmul(weights.T, np.matmul(sigma, weights))
         self.std = math.sqrt(self.var)
+        self.sharpe = (self.mean - universe.rf)/self.std
         portfolio_count += 1
 
     def __str__(self):
@@ -107,13 +109,12 @@ def Markowitz_Risk_Min(univ, expected_return):
     A_inv = np.linalg.inv(A)
     y = np.array([1, expected_return])
     Lagrangian_mult = np.matmul(A_inv, y)
-    
     weights = -1* Lagrangian_mult[0] * np.matmul(sigma_inv, one) - Lagrangian_mult[1]* np.matmul(sigma_inv, mu)
     return portfolio(univ, weights)
 
 def Markowitz_Risk_Min_Vec(univ, expected_return):
     p = Markowitz_Risk_Min(univ, expected_return)
-    return p.std, p.mean
+    return p.std, p.mean, p.sharpe
 
 def Min_Var_Mean(univ):
     one = np.array([1]*len(univ.assets))
@@ -122,29 +123,55 @@ def Min_Var_Mean(univ):
     denominator = np.matmul(one, np.matmul(s_inv, one))
     return numerator/denominator
 
-def bullet_curveMC(universe, resolution, maximum, minimum):
+def Min_Var_Portfolio(univ):
+    return Markowitz_Risk_Min(univ, Min_Var_Mean(univ))
+
+def Min_Var_Portfolio_Vec(univ):
+    return Markowitz_Risk_Min_Vec(univ, Min_Var_Mean(univ))
+
+def Market_Portfolio(univ):
+    sigma_inv = np.linalg.inv(univ.sigma)
+    one = np.array([1]*len(univ.assets))
+    numerator = np.matmul(sigma_inv, mu)
+    denominator = np.matmul(one, np.matmul(sigma_inv, mu))
+    weights = np.array(numerator/denominator)
+    return portfolio(univ, weights)
+
+def Market_Portfolio_Vec(univ):
+    p = Market_Portfolio(univ)
+    return p.std, p.mean, p.sharpe
+
+def bullet_curve(universe, resolution, maximum, minimum):
     assert resolution > 0, "resolution can't be non-negative"
     assert minimum<maximum, "minimum<maximum"
     min_mean = minimum
     max_mean = maximum
     mean_range = max_mean - min_mean 
     returns = []
-
     for i in range(resolution+1):
         returns.append(min_mean + i* mean_range/resolution)
-    
     exp = []
     std = []
+    sharpe = []
     for i in returns:
         p = Markowitz_Risk_Min(univ, i)
         exp.append(p.mean)
         std.append(p.std)
-    return np.array(std), np.array(exp)
+        sharpe.append(p.sharpe)
+    return np.array(std), np.array(exp), np.array(sharpe)
 
-def efficient_frontierMC(universe, resolution, maximum):
+def efficient_frontier(universe, resolution, maximum):
     minimum = Min_Var_Mean(universe)
-    return bullet_curveMC(universe, resolution, maximum, minimum)
+    return bullet_curve(universe, resolution, maximum, minimum)
 
+def capital_allocation_line(universe):
+    b = universe.rf
+    run, rise = Market_Portfolio_Vec(universe)[:2]
+    m = rise/run
+    x = np.linspace(0, max(ef_std), 10)
+    y = m*x+b
+    print(x)
+    return x, y
 
 
 #################
@@ -153,20 +180,19 @@ def efficient_frontierMC(universe, resolution, maximum):
 
 from stockDataClean import stocks, mu, std, sigma
 
-univ = universe(stocks, mu, std, sigma)
+univ = universe(stocks, mu, std, sigma, 0)
 
-ef_std, ef_exp = efficient_frontierMC(univ, 100, .4)
 
-std, exp = bullet_curveMC(univ, 100, .4, .1)
+ef_std, ef_exp, ef_sharpe = efficient_frontier(univ, 1000, .4)
 
-s, e = base_portfolios(univ)
 plt.style.use('seaborn')
-plt.xlabel('std')
-plt.ylabel('return')
+plt.xlabel('Standard Deviation')
+plt.ylabel('Expected Return')
+x,y = capital_allocation_line(univ)
+plt.plot(x, y)
 
-plt.scatter(ef_std, ef_exp, marker = '.')
-plt.scatter(s, e)
-plt.scatter(std, exp, marker = '.')
+plt.scatter(ef_std, ef_exp, c=ef_sharpe, cmap = 'jet', marker = ".", norm = matplotlib.colors.PowerNorm(5))
+plt.colorbar(label = 'Sharpe Ratio',ticks = [1.6, 1.575, 1.55, 1.50, 1.40]).ax.set_ylabel('Sharpe Ratio', rotation = 270, labelpad=25)
 
 plt.show()
 
